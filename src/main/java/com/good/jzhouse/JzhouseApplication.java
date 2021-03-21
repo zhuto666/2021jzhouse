0d8381873b2589a0680e434d3f6c5bc10e173d94
zhuto666 <1944741442@qq.com>
2021-03-21 17:11:05 +0800

    刘志强傻逼

--- src/main/java/com/good/jzhouse/JzhouseApplication.java
+++ src/main/java/com/good/jzhouse/JzhouseApplication.java
@@ -10,6 +10,7 @@ public class JzhouseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JzhouseApplication.class, args);
+        System.out.println();
     }
 
 }
